ddbe9e90cbe82488b42da3d7ecf84856b83ffaa0
andricreative <andrianto@accolaplay.id>
2026-04-30 10:55:28 +0700

    feat: update

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-// app.use(express.static('public')); // Dinonaktifkan karena tidak digunakan
+// app.use(express.static('public'));
 
 // Middleware untuk memastikan DB terkoneksi (Serverless Friendly)
 let isDbConnected = false;
